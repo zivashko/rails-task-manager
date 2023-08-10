9653c2eb3f14c6457fb5a58ddd2f1c7430a25d01
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-11 00:53:46 +0100

    show all tasks and show one task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "tasks", to: "tasks#index"
+  # root "tasks#home"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+
 end
