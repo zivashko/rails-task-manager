5e9ebaf0527fa463cfedc00e7559fa8c13eb1111
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-11 13:33:24 +0100

    As a user I can view the details of task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
 
   def index
+
     @tasks = Task.all
   end
 
   def show
+    @task = Task.new
     @task = Task.find(params[:id])
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "tasks#home"
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
 end
