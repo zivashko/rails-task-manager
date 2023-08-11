35534bd2a5823563e1312d0c579f3398c1243994
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-11 16:56:30 +0100

    the user can edit tasks

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 
 end
