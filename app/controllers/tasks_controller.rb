a065895d453faeed8d1006c45a11f62a0e54ba2b
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-11 18:34:08 +0100

    users can update the tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,48 +1,44 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
-
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.new
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
-  @task = Task.new
-
+    @task = Task.new
   end
 
   def create
-
     @task = Task.new(task_params)
     @task.save
     redirect_to tasks_path
   end
 
-
-  def edit
-
-    @task = Task.find(params[:id])
-
-  end
+  def edit; end
 
   def update
-
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path(@task)
+  end
 
+  def destroy
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
-
+  def set_task
+    @task = Task.find(params[:id])
   end
 
-
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +11,5 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
-
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
