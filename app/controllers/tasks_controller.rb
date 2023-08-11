0262a9354f4fb61b5641411e8cac4452fed6b9dc
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-11 18:36:05 +0100

    refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,39 @@
-# frozen_string_literal: true
-
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
-  def show; end
+  def show
+
+  end
 
   def new
+
     @task = Task.new
   end
 
   def create
+
     @task = Task.new(task_params)
     @task.save
     redirect_to tasks_path
   end
 
-  def edit; end
+  def edit
+
+  end
 
   def update
+
     @task.update(task_params)
     redirect_to tasks_path(@task)
   end
 
   def destroy
+
     @task.destroy
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path, status: :see_other
